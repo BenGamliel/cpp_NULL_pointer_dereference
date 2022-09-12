407c11ba83935d6d41fe653bcc56601d071517ab
BenGamliel <idov3r@gmail.com>
2022-09-12 15:03:15 +0300

    Add files via upload

--- Null_Pointer_Dereference/main.cpp
+++ Null_Pointer_Dereference/main.cpp
@@ -2,79 +2,94 @@
 #include "testClass.h"
 using namespace std;
 testClass * getNullPtr(bool x){
-    cout << " in getNullPtr " << endl;
-   testClass temp = testClass(5);
-   cout << "address of temp: "  << std::dec<< (&temp) << endl;
+//    cout << " in getNullPtr " << endl;
+   testClass* temp = new testClass(5);
+//   cout << "address of temp: "  << std::dec<< (&temp) << endl;
    if(x){
        return nullptr;
    }
-    return &temp;
+    return temp;
 }
 testClass* GetMYTrueObject(){
     testClass* temp = new testClass(3000);
     return temp;
 }
 testClass* GetMYNullObject(){
-    testClass temp = testClass(3);
     return nullptr;
 }
 
 
 int main() {
-    if(testClass* testObject0 = getNullPtr(false)){
-        std::cout << "0" << std::endl << (*testObject0).someFunc() << endl;//NOT a Result
-    }
-    if(testClass* testObject1 = getNullPtr(false); testObject1){
-        std::cout << "1" << std::endl << (*testObject1).someFunc() << endl;//NOT a Result
-    }
-    if(testClass* testObject2 = getNullPtr(true)){
-        std::cout << "2" << std::endl << (*testObject2).someFunc();// NOT a Result
-    }
-    if (testClass* testObject10 = GetMYTrueObject()){
-        std::cout << "10" << std::endl;
-        testObject10->someFunc2();
-    }
-    if (testClass* testObject11 = GetMYNullObject()){
-        std::cout << "11" << std::endl;
-        (*testObject11).someFunc();
-    }
-    if (auto* testObject60 = GetMYTrueObject()){
+
+    if (auto* testObject60 = GetMYTrueObject()){ // NOT a result
         std::cout << "60" << std::endl;
         testObject60->someFunc2();
+        std::cout << endl;
+
     }
-    if (auto* testObject61 = getNullPtr(false)){// FP
+    if (auto* testObject61 = getNullPtr(false)){ // NOT a result
         std::cout << "61" << std::endl;
         testObject61->someFunc2();
     }
-    if (auto* testObject62 = getNullPtr(true)){ // TP
+    if (auto* testObject62 = getNullPtr(true)){ // NOT a result
         std::cout << "62" << std::endl;
         testObject62->someFunc2();
     }
-    if (testClass* const testObject80 = GetMYTrueObject()){
-        std::cout << "80" << std::endl;
-        testObject80->someFunc2();
-    }
-    if (testClass* const testObject81 = getNullPtr(false)){// FP
-        std::cout << "81" << std::endl;
-        testObject81->someFunc2();
-    }
-    if (testClass* const testObject82 = getNullPtr(true)){ // TP
-        std::cout << "82" << std::endl;
-        testObject82->someFunc2();
-    }
-    if (auto* const testObject70 = GetMYTrueObject()){
+    std::cout << endl;
+    if (auto* const testObject70 = GetMYTrueObject()){//SAST result
         std::cout << "70" << std::endl;
         testObject70->someFunc2();
     }
-    if (auto* const testObject71 = getNullPtr(false)){// FP
+    if (auto* const testObject71 = getNullPtr(false)){// FP//SAST result
         std::cout << "71" << std::endl;
         testObject71->someFunc2();
     }
-    if (auto* const testObject72 = getNullPtr(true)){ // TP
+    if (auto* const testObject72 = getNullPtr(true)){ // TP//SAST result
         std::cout << "72" << std::endl;
         testObject72->someFunc2();
     }
+    std::cout << endl;
 
+    if (testClass* const testObject80 = GetMYTrueObject()){//SAST result
+        std::cout << "80" << std::endl;
+        testObject80->someFunc2();
+    }
+    if (testClass* const testObject81 = getNullPtr(false)){//SAST result
+        std::cout << "81" << std::endl;
+        testObject81->someFunc2();
+    }
+    if (testClass* const testObject82 = getNullPtr(true)){//SAST result
+        std::cout << "82" << std::endl;
+        testObject82->someFunc2();
+    }
+    std::cout << endl;
+
+    /*
+if(testClass* testObject0 = getNullPtr(false)){
+    std::cout << "0" << std::endl << (*testObject0).someFunc() << endl;//NOT a Result
+}
+if(testClass* testObject1 = getNullPtr(false); testObject1){
+    std::cout << "1" << std::endl << (*testObject1).someFunc() << endl;//NOT a Result
+}
+if(testClass* testObject2 = getNullPtr(true)){
+    std::cout << "2" << std::endl << (*testObject2).someFunc();// NOT a Result
+}
+if (testClass* testObject10 = GetMYTrueObject()){
+    std::cout << "10" << std::endl;
+    testObject10->someFunc2();
+}
+if (testClass* testObject11 = GetMYNullObject()){
+    std::cout << "11" << std::endl;
+    (*testObject11).someFunc();
+}
+ */
+    /*
+     *     if(testClass* testObject3 = getNullPtr(true); true){
+        std::cout << "3" << std::endl << (*testObject3).someFunc();//RESULT
+    }
+    else{
+        std::cout << "3.1" << std::endl << (*testObject3).someFunc() << endl;// NOT a Result
+    }
     if(testClass* testObject32 = getNullPtr(true); false){
         std::cout << "3.2" << std::endl << (*testObject32).someFunc();//NOT a result
     }
@@ -98,12 +113,8 @@ int main() {
         std::cout << "61" << std::endl;
         (*testObject61).someFunc();
     }
-    if(testClass* testObject3 = getNullPtr(true); true){
-        std::cout << "3" << std::endl << (*testObject3).someFunc();//RESULT
-    }
-    else{
-        std::cout << "3.1" << std::endl << (*testObject3).someFunc() << endl;// NOT a Result
-    }
+
+     */
     return 0;
 }
 
